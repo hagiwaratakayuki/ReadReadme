39c7e5c3b570fe9029db61addd969a3df26776db
hagiwaratakayuki <hagiwaratakayuki@gmail.com>
2022-08-03 12:05:56 +0900

    fix for go mod blacket

--- language/go.js
+++ language/go.js
@@ -6,12 +6,12 @@ const QUATE = /(["'])(.+)?\1/;
 const NOT_CLOSE_PT = /["'][^"'\s]+ | [^"'\s]+["']/;
 const CLOSER_PT = /["']/g;
 const ERRORS = require('../error_messages.js');
-
+const URL_REGX = /[\w!\?/\+\-_~=;\.,\*&@#\$%\(\)'\[\]]+/
 
 let UPDATE;
 let GO_MOD_PATH;
 let LIBRARY_URLS;
-let RENAME_MAP;
+let REPLACE_MAP;
 let IS_GO_PATH = false;
 
 
@@ -70,23 +70,63 @@ class Engine extends Base {
                 if (IS_GO_PATH === false) {
                     const lines =  new TextDecoder().decode(await vscode.workspace.fs.readFile(GO_MOD_PATH)).split(/\r|\r\n|\n/)
                     LIBRARY_URLS = [];
-                    RENAME_MAP = {};
+                    REPLACE_MAP = {};
+                    let isRequire = false;
+                    let isRequireModeIn = false
+                    let isReplace = false;
+                    let isReplaceModeIn = false
+
                     for (let line of lines) {
-                        let trimed = line.trim();
-                        
+                        isRequireModeIn = false
+                        isReplaceModeIn = false;
+                        let splited = line.trim().split('//');
+                        if (splited.length > 1) {
+                            splited.pop();
 
+                        }
+                        let trimed = splited.join('//');
                         if (trimed.indexOf('require') === 0) {
-                            LIBRARY_URLS.push(trimed.split(/\s+/)[1].replace(/\\$/, ''))
+                            isRequire = true;
+                            trimed = trimed.replace('require', '').trim()
+                            isRequireModeIn = true;
+                        }
 
-
+                        if (isRequire) {
+                            let cand = URL_REGX.exec(trimed);
+                            if (cand.length > 0 && cand[0].includes('.') === true && cand[0].includes('/') === true) {
+                                let url = cand[0].split('/v[\d+.]/i')[0]
+                                LIBRARY_URLS.push(url)
+                                trimed = trimed.replace(url, '');
+                            }
+                            if (trimed.includes(')') === true ||  ( isRequireModeIn === true && trimed.includes('(') === false) ) {
+                                isRequire = false;
+                            }
 
                         }
-                        if (trimed.indexOf('replace') === 0) {
-                            let {left, right} = trimed.replace('replace', '').trim().split(/\s*=>\s*/)
-                            let baseVersion = left.split(/\s+/)[0].replace(/\\$/, '');
-                            let replace = right.trim().split(/\s+/)[0];
-                            RENAME_MAP[baseVersion] = replace; 
+
+
+                        
+                        
+                        if (trimed.includes('replace') === 0) {
+                            isReplaceModeIn = isReplace = true;
+                            trimed = trimed.replace('replace', '');
+                            
+
+                             
                         }
+
+                        if (isReplace) {
+                            if (trimed.includes('=>')) {
+                                let { left, right } = trimed.replace('replace', '').trim().split(/\s*=>\s*/)
+                                let baseVersion = left.split(/\s+/)[0].replace(/\\$/, '');
+                                let replace = right.trim().split(/\s+/)[0];
+                                REPLACE_MAP[baseVersion] = replace;
+                            }
+                            if (trimed.includes(')') === true || (isReplaceModeIn === true && trimed.includes('(') === false)) {
+                                isReplace = false;
+                            }                         
+                        }
+
                         
                     }
                 }
@@ -94,10 +134,10 @@ class Engine extends Base {
             }
             if (IS_GO_PATH === false) {
                 let isReplaced = false;
-                for (let baseVersion of Object.keys(RENAME_MAP)) {
+                for (let baseVersion of Object.keys(REPLACE_MAP)) {
                     let check = reguraisedText.replace(baseVersion, '');
                     if (!check || check.indexOf('/') === 0) {
-                        let replace = RENAME_MAP[baseVersion];
+                        let replace = REPLACE_MAP[baseVersion];
                         if (replace.indexOf('.') === 0) {
                             return ERRORS.AppCode;
                         }
